a1fd261db2762d7d50e4f00d2e94493219ae329e
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2022-07-20 23:11:46 +0530

    till movielist

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 const MovieList = (props) => {
 	const FavouriteComponent = props.favouriteComponent;
+
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
-					{/* <div className='overlay d-flex align-items-center justify-content-center'>
+				<div className='image-container d-flex m-3'>
+					<img className='img' src={movie.Poster} alt='movie'></img>
+				    <div
+						onClick={() => props.handleFavouritesClick(movie)}
+						className='overlay d-flex align-items-center justify-content-center'
+					>
 						<FavouriteComponent />
-					</div> */}
+					</div> 
 				</div>
 			))}
 		</>
